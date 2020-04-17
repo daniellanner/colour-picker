34a6c57a6123fad04caacef267ed3eea6c7cc026
Daniel Lanner <lanner.dan@gmail.com>
2020-04-16 22:03:57 -0400

    Restaging repo

--- Colour Picker/Assets/Scripts/IChangeActiveColourChannelButtonVisual.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IChangeActiveColourChannelButtonVisual
-{
-    void Selected(ChangeActiveColourChannelButton.TypeOfColourChannel p_selectedChannel);
-}
